f85379099c42a422cf4346028c8686c46c115203
joey00072 <shubhamchoudhari00072@gmail.com>
2021-05-19 14:59:58 +0530

    loops done 👍

--- nodes/loop.py
+++ nodes/loop.py
@@ -1,20 +1,24 @@
 class ForNode:
-	def __init__(self, var_name_token, start_value_node, end_value_node, step_value_node, body_node):
-		self.var_name_token = var_name_token
-		self.start_value_node = start_value_node
-		self.end_value_node = end_value_node
-		self.step_value_node = step_value_node
-		self.body_node = body_node
+    def __init__(self, var_name_token, start_value_node, end_value_node, step_value_node, body_node):
+        self.var_name_token = var_name_token
+        self.start_value_node = start_value_node
+        self.end_value_node = end_value_node
+        self.step_value_node = step_value_node
+        self.body_node = body_node
 
-		self.pos_start = self.var_name_token.pos_start
-		self.pos_end = self.body_node.pos_end
+        self.pos_start = self.var_name_token.pos_start
+        self.pos_end = self.body_node.pos_end
 
+    def __repr__(self):
+        return f"For Loop[{self.start_value_node},{self.end_value_node},{self.step_value_node}] :{self.body_node}"
 
 
 class WhileNode:
-	def __init__(self, condition_node, body_node):
-		self.condition_node = condition_node
-		self.body_node = body_node
+    def __init__(self, condition_node, body_node):
+        self.condition_node = condition_node
+        self.body_node = body_node
 
-		self.pos_start = self.condition_node.pos_start
-		self.pos_end = self.body_node.pos_end   
+        self.pos_start = self.condition_node.pos_start
+        self.pos_end = self.body_node.pos_end   
+    def __repr__(self):
+        return f"WHILE {self.body_node}"
